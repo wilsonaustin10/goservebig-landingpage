aa457cecb139827391c863c546374cfcdd5f9eee
Austin Wilson <wilson.austin10@gmail.com>
2025-05-05 09:19:38 -0500

    Updated phone number

--- src/app/property-listed/page.tsx
+++ src/app/property-listed/page.tsx
@@ -30,7 +30,7 @@ export default function PropertyListedPage() {
 
             <div className="mb-12">
               <Link
-                href="tel:(732) 800-5097"
+                href="tel: (732) 800-5097"
                 className="inline-block bg-secondary hover:bg-opacity-90 text-black px-8 py-4 rounded-md text-xl font-medium transition-colors"
               >
                 Call Us Now: (732) 800-5097
